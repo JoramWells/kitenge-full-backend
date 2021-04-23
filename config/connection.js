d19316eabd462f75c0edc5b5f72575967354f638
unknown <jorammanoah1@gmail.com>
2021-04-23 13:50:07 -0700

    Added app.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-module.exports = new Sequelize('kitenge','root','',{
+module.exports = new Sequelize('kitenge','root','JoramWells18.',{
     host:'localhost',
     dialect:'mysql',
 })
